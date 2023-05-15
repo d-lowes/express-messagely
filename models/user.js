9dfa36a3ecc3f8c9c7fda9f287803a5d80526ea0
MKim <mrn.kim07@gmail.com>
2023-05-15 11:45:53 -0700

    create register and authenticate functions under User class

--- models/user.js
+++ models/user.js
@@ -2,30 +2,54 @@
 
 /** User of the site. */
 
-class User {
+const { NotFoundError } = require("../expressError");
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+const { SECRET_KEY, BCRYPT_WORK_FACTOR } = require("../config");
+
+class User {
   /** Register new user. Returns
    *    {username, password, first_name, last_name, phone}
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(
+      `INSERT INTO users (username, password, first_name, last_name, phone)
+    VALUES
+      ($1, $2, $3, $4, $5)
+    RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone]
+    );
+
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT password
+         FROM users
+         WHERE username = $1`,
+      [username]
+    );
+    const user = result.rows[0];
+
+    return user && (await bcrypt.compare(password, user.password)) === true;
   }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) {
-  }
+  static async updateLoginTimestamp(username) {}
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
-  static async all() {
-  }
+  static async all() {}
 
   /** Get: get user by username
    *
@@ -36,8 +60,7 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) {
-  }
+  static async get(username) {}
 
   /** Return messages from this user.
    *
@@ -47,8 +70,7 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) {
-  }
+  static async messagesFrom(username) {}
 
   /** Return messages to this user.
    *
@@ -58,9 +80,7 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) {
-  }
+  static async messagesTo(username) {}
 }
-
 
 module.exports = User;
